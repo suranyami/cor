2bb98ce74571ca918d2cb64d60d8121afbeaf390
David Parry <david.parry@suranyami.com>
2014-09-27 09:00:55 +1000

    Using renamed hexate library, because `hex` clashed with `Hex.pm`
    Was exhibiting **very** strange behaviour when running `mix test`.
    Would work fine first time, then couldn't find functions the second time.
    Cause was likely to do with Hex.pm being integrated into Elixir nowadays.

--- mix.exs
+++ mix.exs
@@ -8,23 +8,13 @@ defmodule Cor.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:hexate,  ">= 0.5.0", [github: "suranyami/hexate"]},
+    ]
   end
 end
